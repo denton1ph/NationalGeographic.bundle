1a79290eff28c0c09f8b3c8431c1aa09f26a4782
shopgirl284 <shopgirl284@gmail.com>
2016-05-15 15:20:22 -0400

    Update for Video pull
    The json for videos is no longer available. This uses the webites html

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,121 +1,105 @@
+NAME = 'National Geographic'
+VID_PREFIX = '/video/nationalgeographic'
+PHOTO_PREFIX = '/photos/nationalgeographic'
+
 POD_FEED = "http://feeds.nationalgeographic.com/ng/photography/photo-of-the-day/"
 BASE_URL = "http://video.nationalgeographic.com"
-JSON_CAT_URL = "http://video.nationalgeographic.com/video/player/data/mp4/json/main_sections.json"
-JSON_CHANNEL_CAT_URL = "http://video.nationalgeographic.com/video/player/data/mp4/json/category_%s.json"
-JSON_PLAYLIST_URL = "http://video.nationalgeographic.com/video/player/data/mp4/json/lineup_%s_%s.json"
-#JSON_VIDEO_URL = "http://video.nationalgeographic.com/video/player/data/mp4/json/video_%s.json"
-
-NAME = L('Title')
-RE_DURATION = Regex('(?P<mins>[0-9]+):(?P<secs>[0-9]+)')
 
 ####################################################################################################
 def Start():
 
-	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-	Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
-
 	# Set the default ObjectContainer attributes
 	ObjectContainer.title1 = NAME
-	ObjectContainer.view_group = "List"
 
 	# Set the default cache time
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:20.0) Gecko/20100101 Firefox/20.0"
 
 ####################################################################################################
-@handler('/video/nationalgeographic', L('VideoTitle'))
+@handler(VID_PREFIX, NAME + ' Videos')
 def VideosMainMenu():
 
 	oc = ObjectContainer()
 
 	# Iterate over all of the available categories and display them to the user.
-	categories = JSON.ObjectFromURL(JSON_CAT_URL)
-	for category in categories['sectionlist']['section']:
-		name = category['label'].replace(' Video', '')
-		oc.add(DirectoryObject(key = Callback(ChannelVideoCategory, id = category['id'], name = String.DecodeHTMLEntities(name)), title = name))
+	oc.add(DirectoryObject(key = Callback(VideoCategory, title = 'All', url = BASE_URL), title = 'All'))
+	categories = HTML.ElementFromURL(BASE_URL).xpath('//section[@id="grid-container"]//ul[@class="dropdown-menu"]/li/a')
+	for category in categories:
+		url = BASE_URL + category.xpath('./@href')[0]
+		name = category.xpath('./text()')[0]
+		oc.add(DirectoryObject(key = Callback(VideoCategory, title = name, url = url), title = name))
 
 	return oc
 
 ####################################################################################################
-@route('/video/nationalgeographic/{id}')
-def ChannelVideoCategory(id, name, parent=''):
+@route(VID_PREFIX + '/category')
+def VideoCategory(url, title):
 
-	oc = ObjectContainer()
+	oc = ObjectContainer(title2=title)
 	
-	# Iterate over all the subcategories. It's possible that we actually find another sub-sub-category
-	# In this case, we will simply recursively call this function again until we find actual playlists.
-	sub_categories = JSON.ObjectFromURL(JSON_CHANNEL_CAT_URL % id)
-	for sub_category in sub_categories['section']['children']:
-		name = String.DecodeHTMLEntities(sub_category['label'])
+	# Iterate over all the subcategories
+	sub_categories = HTML.ElementFromURL(url).xpath('//section[@id="grid-container"]//ul[contains(@class, "grid-sections")]/li/a')
+	for sub_category in sub_categories:
+		# For some reason @href is picking up a lot of junk not seen in the code, so we have to cut it down
+		url_ext = sub_category.xpath('./@href')[0].split('gs=')[1]
+		section_url = '%s?gs=%s' %(url, url_ext)
+		name = sub_category.xpath('./text()')[0]
+		oc.add(DirectoryObject(key = Callback(VideoPlaylist, title = name, url = section_url), title = name))
 
-		has_child = sub_category['hasChild']
-		if has_child == "true":
-			oc.add(DirectoryObject(key = Callback(ChannelVideoCategory, id = sub_category['id'], name = name, parent=parent+'/'+id), title = name))
-		else:
-			oc.add(DirectoryObject(key = Callback(ChannelVideoPlaylist, id = sub_category['id'], name = name, parent=parent+'/'+id), title = name))
-
-	# It's possible that there is actually no vidoes are available for the ipad. Unfortunately, they
-	# still provide us with empty containers...
 	if len(oc) < 1:
-		return ObjectContainer(header=name, message="There are no titles available for the requested item.")
-
-	return oc
+		return ObjectContainer(header=name, message="There are no sections available for the requested item.")
+	else:
+		return oc
 
 ####################################################################################################
-@route('/video/nationalgeographic/{id}/playlist', page = int, allow_sync = True)
-def ChannelVideoPlaylist(id, name, parent='', page=0):
+@route(VID_PREFIX + '/playlist', page = int)
+def VideoPlaylist(url, title, page=0):
 
-	oc = ObjectContainer(view_group="InfoList")
+	oc = ObjectContainer(title2=title)
 
-	# Unable to properly resolve the urls from the json, so from this point on we us html
-	local_url = '%s/video/%s/%s/%s/' %(BASE_URL, parent, id, str(page))
-	section_name = name
+	local_url = '%s&gp=%s' %(url, str(page))
+	section_name = title
 	data = HTML.ElementFromURL(local_url)
-	for video in data.xpath('//ul[contains(@class,"grid")]/li/div[@class="vidthumb"]'):
-		try: lock = video.xpath('./span[@class="vidtimestamp"]/span/@class')[0]
-		except: lock = None
-		# Skip locked videos
-		if lock:
+	for video in data.xpath('//section[@id="grid-container"]//div[@class="media-module"]'):
+		# Again adding junk to url so delete the extension here
+		vid_url = BASE_URL + video.xpath('./a/@href')[0].split('?gc=')[0]
+		# Skip links that are not videos
+		if not '/video/' in vid_url:
 			continue
-		url = BASE_URL + video.xpath('./a/@href')[0]
-		if not url.startswith("http://"):
-			url = BASE_URL + url
-		thumb = video.xpath('.//img/@src')[0]
+		if not vid_url.startswith("http://"):
+			vid_url = BASE_URL + vid_url
+		thumb = video.xpath('.//img/@data-src')[0]
 		if not thumb.startswith("http://"):
 			thumb = BASE_URL + thumb
-		name = String.DecodeHTMLEntities(video.xpath('./a/@title')[0])
-		duration = video.xpath('./span[@class="vidtimestamp"]//text()')[0].strip()
+		name = video.xpath('./a/@data-title')[0]
+		duration = video.xpath('./div[@class="timestamp"]//text()')[0].strip()
 		try: duration = Datetime.MillisecondsFromString(duration)
 		except: duration = None
 
 		
 		oc.add(VideoClipObject(
-			url = url, 
+			url = vid_url, 
 			title = name, 
 			thumb = thumb,
 			duration = duration
 		))
 
 	# Paging
-	pages = data.xpath('.//nav[contains(@class, "pagination")]/li/a//text()')
-	for item in pages:
-		if 'Next' in item:
-			page = page + 1
-			oc.add(NextPageObject(key = Callback(ChannelVideoPlaylist, id=id, parent=parent, name=section_name, page=page), title = L("Next Page ...")))
+	try: page = int(data.xpath('//a[contains(@class, "load-more")]/@href')[0].split('gp=')[1])
+	except: page = None
+	if page:
+		oc.add(NextPageObject(key = Callback(VideoPlaylist, url=url, title=section_name, page=page), title = L("Next Page ...")))
 	
-	# It's possible that there is actually no vidoes are available for the ipad. Unfortunately, they
-	# still provide us with empty containers...
 	if len(oc) < 1:
-		return ObjectContainer(header=name, message="There are no videos available for this category.")
-	
-	return oc
+		return ObjectContainer(header='Empty', message="There are no videos available for this category.")
+	else:
+		return oc
 
 ####################################################################################################
-@handler('/photos/nationalgeographic', L('PhotosTitle'))
+@handler(PHOTO_PREFIX, NAME + ' Photos')
 
 def PhotosMainMenu():
-	oc = ObjectContainer(view_group='Pictures')
+	oc = ObjectContainer()
 	
 	feed = XML.ElementFromURL(POD_FEED, errors='ignore')
 	for item in feed.xpath('//item'):
